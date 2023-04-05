22efb8baf57dfb165d40ee93fa01cbf0f9ad4dc5
septiiadewii <110364396+septiiadewii@users.noreply.github.com>
2023-04-05 20:06:40 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,29 @@
+<div class="container">
+    <h4 class="text-center mt-3">Uji Kompetensi Keahlian <br> Rekayasa Perangkat Lunak (RPL) <br> Tahun 2022/2023</h4>
+    <hr>
+    <div class="row mt-3">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header" style="background-color: #a0ff99 ">
+                    INFORMASI
+                </div>
+                <div class="card-body">
+                    Aplikasi pengaduan masyarakat ini dibuat untuk memenuhi tugas Uji Kompetensi Keahlian RPL Tahun 2023<br><br>
+                </div>
+                <div class="card-footer" style="background-color: #a0ff99"></div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header" style="background-color: #a0ff99">KONTAK</div>
+                <div class="card-body">
+                    Dikembangkan oleh <br>
+                    Nama : Septia Dewi <br>
+                    Kelas : 12 RPL 1 <br>
+                    No. Hp : Cari di Goggle <br>
+                </div>
+                <div class="card-footer" style="background-color: #a0ff99"></div>
+            </div>
+        </div>
+    </div>
+</div>
